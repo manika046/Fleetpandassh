32d91abe5819b486f2bb077a97f6112f928bae15
manika046 <kasulamanika00@gmail.com>
2024-07-19 17:50:22 +0545

    second commit

--- sheet.js
+++ sheet.js
@@ -30,15 +30,54 @@ function validateform() {
         document.getElementById('merror').innerText = "";
     }
 
-    if(isvalid){
+    if (isvalid) {
         document.getElementById("form").onsubmit
     }
 
-    const formdata= {
-        name : name,
-        email : email,
-        message : message,
+    const formdata = {
+        name: name,
+        email: email,
+        message: message,
     };
     alert(JSON.stringify(formdata));
-    console.log(formdata);
+}
+
+function Login(event) {
+    event.preventDefault();
+
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+
+    var isValid = true;
+
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (email === "") {
+        document.getElementById("emailerror").innerText = "Email is required.";
+        isValid = false;
+    } else if (!emailPattern.test(email)) {
+        document.getElementById("emailerror").innerText = "Invalid email format.";
+        isValid = false;
+    } else {
+        document.getElementById("emailerror").innerText = "";
+    }
+
+    if (password === null || password === "") {
+        document.getElementById('perror').innerText = "Enter password";
+        isValid = false;
+    } else {
+        document.getElementById('perror').innerText = "";
+    }
+
+    if (isValid) {
+        // const formdata = {
+        //     email: email,
+        //     password: password,
+        // };
+        // alert(JSON.stringify(formdata));
+        alert("login");
+        // document.getElementById("form").submit(); // Submit the form programmatically if validation is successful
+
+        window.location = "index.html";
+    }
+    
 }
